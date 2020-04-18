fd965af6b383c63510f8c2ac6c2fa79320b82300
hello-consumer <hello-consumer@users.noreply.github.com>
2020-04-18 16:25:41 -0500

    Imported sticky navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
-  return (
-    <Button variant="contained" color="primary">Hello World</Button>
+  
+  return (<React.Fragment>
+    <CssBaseline />
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">
+          Clincher
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container style={{height: '100vh'}}>
+        <Typography component="div" >
+          Lorem ipsum....
+        </Typography>
+        <Button variant="contained" color="primary">Hello World</Button>
+    </Container>
+    
+  </React.Fragment>
   );
 }
 
